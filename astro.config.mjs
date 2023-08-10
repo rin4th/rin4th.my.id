a51eb916e4f38cc01e803e2345de8930ab9b1955
Rin4th <rizalnur.art@gmail.com>
2023-08-10 20:31:46 +0700

    Update: config of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,18 @@ import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [Unocss(), svelte(), mdx(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  })],
-  site: "https://rin4th.my.id",
-  vite: {
-    resolve: {
-      alias: {
-        "~/*": path.resolve("src")
-      }
-    },
-    ssr: {
-      external: ["svgo"]
-    }
-  },
-  output: "server",
-  adapter: cloudflare()
+	integrations: [Unocss(), svelte(), mdx(), image({ serviceEntryPoint: "@astrojs/image/sharp" })],
+	site: "https://rin4th.my.id",
+	vite: {
+		resolve: {
+			alias: {
+				"~/*": path.resolve("src"),
+			}
+		},
+		ssr: {
+			external: ["svgo"],
+		},
+	},
 });
